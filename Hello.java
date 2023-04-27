631bf44a49b1590bf758c77d8e835b4ae5bf33f3
Harshithap14 <92721145+Harshithap14@users.noreply.github.com>
2023-04-27 16:14:24 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-public class Main
+class Test
 {
-  public static void main(String[] args)
-  {
-    System.out.println("Hello, World!");
-  }
-}
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
